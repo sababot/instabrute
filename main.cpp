01d76cb9644c2e2415d0544e338724ad8fe95044
sababot <twinsgardos@gmail.com>
2021-11-01 00:00:07 +0100

    asdf

--- main.cpp
+++ main.cpp
@@ -8,10 +8,20 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	string username = argv[1];
-	string password = argv[2];
-	string command = "python3 scripts/ig.py " + username + " " + password;
-	cout << command << endl;
-	const char *command_current = command.c_str();
-	system(command_current);
+	string password1 = argv[2];
+	string password2 = argv[2];
+	string password3 = argv[2];
+	string command1 = "python3 python/main.py " + username + " " + password1;
+	string command2 = "python3 python/main.py " + username + " " + password2;
+	string command3 = "python3 python/main.py " + username + " " + password3;
+	cout << command1 << endl;
+	cout << command2 << endl;
+	cout << command3 << endl;
+	const char *command_current1 = command1.c_str();
+	const char *command_current2 = command2.c_str();
+	const char *command_current3 = command3.c_str();
+	system(command_current1);
+	system(command_current2);
+	system(command_current3);
 	return 0;
 }
