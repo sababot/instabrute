4fc404378063e9512c2e8fd560f737a1ea4beef1
sababot <twinsgardos@gmail.com>
2021-11-01 00:46:00 +0100

    several instances

--- main.cpp
+++ main.cpp
@@ -11,17 +11,8 @@ int main(int argc, char** argv)
 	string password1 = argv[2];
 	string password2 = argv[2];
 	string password3 = argv[2];
-	string command1 = "python3 python/main.py " + username + " " + password1;
-	string command2 = "python3 python/main.py " + username + " " + password2;
-	string command3 = "python3 python/main.py " + username + " " + password3;
-	cout << command1 << endl;
-	cout << command2 << endl;
-	cout << command3 << endl;
-	const char *command_current1 = command1.c_str();
-	const char *command_current2 = command2.c_str();
-	const char *command_current3 = command3.c_str();
-	system(command_current1);
-	system(command_current2);
-	system(command_current3);
+	string command = "python3 python/main.py " + username + " " + password1 + " & python3 python/main.py " + username + " " + password2 + " & python3 python/main.py " + username + " " + password3;
+	const char *command_current = command.c_str();
+	system(command_current);
 	return 0;
 }
